b9fd2c5bc4df575c389a83cc0d0e4eb9f64c911a
Rayshield21 <35118778+Rayshield21@users.noreply.github.com>
2018-01-10 01:55:45 -0700

    Add files via upload

--- reverseString.js
+++ reverseString.js
@@ -1,9 +1,10 @@
+
 function reverseString(str) {
   var string=str;
   var array=[];
-  var reducer = function reducer(accumulator, currentValue){ 
+  var reducer = function reducer(accumulator, currentValue){
     return accumulator + currentValue;};
-  for(i=string.length-1;i>=0;i--){ 
+  for(i=string.length-1;i>=0;i--){
     array.push(string[i]);
   }
   return array.reduce(reducer);
